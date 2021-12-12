194c033b6a5eb9ff4d4aed616700729fcb8e46b7
Alex M <milesAwayAlex@gmail.com>
2021-12-11 19:52:31 -0800

    Set up basic routing

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product, products } from '../products';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+  styleUrls: ['./product-details.component.css'],
 })
 export class ProductDetailsComponent implements OnInit {
+  product: Product | undefined;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    // extract the product ID from the current route
+    const routeParams = this.route.snapshot.paramMap;
+    const productIDFromRoute = Number(routeParams.get('productId'));
+    // set this.product to the corresponding product
+    this.product = products.find(({ id }) => id === productIDFromRoute);
   }
-
 }
